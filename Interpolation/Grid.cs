891b4f37fe077a6c4683cb0a543b30872b49da38
E.Z. Hart <hartez@gmail.com>
2021-08-11 21:52:29 -0600

    Initial

---
+++ Interpolation/Grid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+	public static class Grid
+	{
+		public static int GetColumn() 
+		{
+			return 0;
+		}
+
+		public static int GetRow()
+		{
+			return 0;
+		}
+
+		public static int GetColumnSpan()
+		{
+			return 1;
+		}
+
+		public static int GetRowSpan()
+		{
+			return 1;
+		}
+	}
+}
---
+++ Interpolation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Interpolation;
+
+Console.WriteLine("Hello, World!");
+
+
+var id = 0;
+
+while (id < 4) 
+{
+	var expected = $"{id++}: " +
+						$"{Grid.GetColumn()}x{Grid.GetRow()} " +
+						$"{Grid.GetColumnSpan()}x{Grid.GetRowSpan()}";
+
+	Console.WriteLine(expected);
+}
